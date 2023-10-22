bac9b4a9460742c6a9e73298af14e2ec89147c5f
Фидан Мамедова <gold_wolverine@mail.ru>
2023-10-22 22:36:36 +0400

    Done

--- project-react-store/src/Main/MyBag.jsx
+++ project-react-store/src/Main/MyBag.jsx
@@ -99,6 +99,8 @@ export function MyBag() {
     setPhoneNumberValidation("");
     setAddressValidation("");
     setSubmitFormIsVisible(false);
+    setOrderSuccess(false);
+    dispatch(clearBasket())
   };
 
   const handleSuccessSubmit = () => {
